82112ecf3e581f2ca3a8002764699d8b8ed1ab0b
ijwo <ijwo@fetchrobotics.com>
2019-04-04 17:30:00 -0700

    made clustering in perception a nodelet

--- fetch_demos_common/src/clustering_node.cpp
+++ fetch_demos_common/src/clustering_node.cpp
@@ -1,48 +1,34 @@
-// an actoin sever that sends object message 
+#include <fetch_demos_common/clustering_node.h>
+#include <pluginlib/class_list_macros.h>
 
-#include <iostream>
-// ROS 
-#include <ros/ros.h>
-#include <actionlib/server/simple_action_server.h>
-#include <fetch_demos_common/perception_clustering.h>
-#include <fetch_demos_common/GetObjectsAction.h>
-
-// PCL
-
-class PerceptionNode
+namespace fetch_demos_common
 {
-    typedef actionlib::SimpleActionServer<fetch_demos_common::GetObjectsAction> server_t;
-public:
-    PerceptionNode(ros::NodeHandle n)
+
+    PerceptionNode::PerceptionNode()
     {
-        server_.reset(new server_t(n, "perception_cluser_objs", 
+    }
+    PerceptionNode::~PerceptionNode()
+    {
+    }
+    void PerceptionNode::onInit()
+    {
+        NODELET_DEBUG("Initializing nodelet...");
+        ros::NodeHandle nh("~");
+        server_.reset(new server_t(nh, "perception_cluser_objs", 
                                    boost::bind(&PerceptionNode::cb, this, _1),
                                    false));
-        clusterer_.reset(new fetch_demos::PerceptionClustering(n));
+        clusterer_.reset(new fetch_demos_perception::PerceptionClustering(nh));
         server_->start();
+        
     }
 
-private:
-    void cb(const fetch_demos_common::GetObjectsGoalConstPtr& goal)
+    void PerceptionNode::cb(const fetch_demos_common::GetObjectsGoalConstPtr& goal)
     {
         fetch_demos_common::GetObjectsResult result;
         clusterer_->getObjects(result);
         server_->setSucceeded(result);
 
     }
-    boost::shared_ptr<server_t> server_;
-    boost::shared_ptr<fetch_demos::PerceptionClustering> clusterer_;
-
-};
-
-int main(int argc, char* argv[]){
-    ros::init(argc, argv, "fetch_demos_clustering");
-    ros::NodeHandle nh("~");
-    PerceptionNode perception(nh);
-    ros::Duration(3.0).sleep();
-
-    // add a subsriber here for receiveing the registered point cloud
-    ros::spin();
-    return 0;
 
 }
+PLUGINLIB_EXPORT_CLASS(fetch_demos_common::PerceptionNode, nodelet::Nodelet)
